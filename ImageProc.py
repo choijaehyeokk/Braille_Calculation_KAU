34cd743549a9c2f05247b3f9653292144ad9809b
lsw725 <725lsw@hanmail.net>
2020-10-10 19:39:51 +0900

    Modify ImageProc.py

--- ImageProc.py
+++ ImageProc.py
@@ -1,33 +1,156 @@
-import cv2 as cv
+from cv2 import cv2 as cv
 import numpy as np
+import os
 
-def cutting(imgPath):
-    img = cv.imread(imgPath,cv.IMREAD_UNCHANGED)
-    if(img is None):
-        return
-    size = np.size(img)
-    shape = np.shape(img)
+class ImgProc:
+    ''' 
+        Image processing for predict
+    '''
 
-    img_cp = np.copy(img)
+    def __init__(self,imgPath):
+        '''
+        imgPath: 이미지 경로
+        width: 이미지 넓이
+        height: 이미지 높이
+        length: 잘려질 이미지 수
+        originImg: 이미지 경로속 이미지의 배열
+        predictDir: 예측 디렉토리 경로
+        dividedImg: 잘려진 이미지의 배열들을 원소로 갖는 list
+        '''
+        self.imgPath = imgPath
+        self.width = 0
+        self.height = 0
+        self.length = 0
+        self.originImg = None
+        self.predictDir = ""
+        self.dividedImg = None
 
-    braile_letters = list()
-    for i in range(1,shape[1]//shape[0]+1):
-        cv.line(img_cp,(shape[0]*i,0),(shape[0]*i,shape[0]),(0,0,0),3)
-        braile_letters.append(img[:,shape[0]*(i-1):shape[0]*i])
+    def setImg(self):
+        '''
+            imgPath의 경로에 있는 이미지를 가져와 객체에 저장한다.
+        '''
+        self.originImg = cv.imread(self.imgPath,cv.IMREAD_UNCHANGED)
+        shape = np.shape(self.originImg)
+        self.height = shape[0]
+        self.width = shape[1]
+        self.length = shape[1]//shape[0]
+
+    def createPredictDir(self):
+        '''
+            예측을 위한 디렉토리를 만들어 predictDir에 저장한다.
+        '''
+        path = './assets/predict/'
+        try:
+            try:
+                os.mkdir(path)
+            except:
+                pass
+            print('예측을 위한 디렉토리 생성')
+        except:
+            print('이미 디렉토리가 존재합니다.')
+            pass
+        self.predictDir = path
+
+    def removePredictDir(self):
+        '''
+            예측을 위한 디렉토리와 그 안에 있는 모든 파일과 디렉토리를 삭제한다.
+        '''
+        try:
+            for root, dirs, files in os.walk(self.predictDir, topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+            os.rmdir(self.predictDir)
+            self.predictDir = ""
+            self.dividedImg = None
+            print("예측을 위한 디렉토리 삭제 완료")
+        except:
+            print('삭제에 실패')
+
+    def checkPredictDir(self):
+        '''
+            예측을 위한 디렉토리가 있는지 확인한다.
+        '''
+        if self.predictDir == "":
+            return False
+        else:
+            return True
+
+    def cutting(self):
+        '''
+            예측을 위해 원본 이미지를 잘라 예측 가능한 이미지로 만든다.
+        '''
+        try:
+            if not self.checkPredictDir():
+                print("예측을 위한 디렉토리가 없음")
+                return
+
+            self.dividedImg = list()
+            for i in range(0,self.length):
+                cv.imwrite(self.predictDir + "/" + str(i) + ".png", self.originImg[:,self.height*i:self.height*(i+1)])
+                self.dividedImg.append(self.originImg[:,self.height*i:self.height*(i+1)])
+        except:
+            print("예외 발생")
     
-    return braile_letters
+    def checkOrigin(self):
+        '''
+            원본이미지를 출력한다.
+        '''
+        if self.originImg is not None:
+            cv.imshow("Origin", self.originImg)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+        else:
+            print("셋팅된 이미지가 없습니다.")
+
+    def checkPredict(self,index):
+        '''
+            예측 가능한 이미지를 출력한다.
+            index: 몇번째 예측 가능한 이미지를 출력할지 정하는 매개변수,
+                    -1일 경우 모든 예측 가능한 이미지를 출력한다.
+        '''
+        try:
+            if index == -1:
+                for i in range(self.length):
+                    img = cv.imread(self.predictDir + "/" + str(i) + ".png", cv.IMREAD_UNCHANGED)
+                    cv.imshow("Predict", img)
+                    cv.waitKey(0)
+                    cv.destroyAllWindows()
+            else:
+                img = cv.imread(self.predictDir + "/" + str(index) + ".png", cv.IMREAD_UNCHANGED)
+                cv.imshow("Predict", img)
+                cv.waitKey(0)
+                cv.destroyAllWindows()
+        except:
+            print("셋팅된 이미지가 없습니다.")
+
+    def __str__(self):
+        '''
+            현재 객체 정보를 출력한다.
+        '''
+        text = "imgPath: " + str(self.imgPath) + "\n" +\
+               "width: " + str(self.width) + "\n" +\
+               "height: " + str(self.height) + "\n" +\
+               "length: " + str(self.length) + "\n" +\
+               "originImg: " + str(self.originImg) + "\n" + \
+               "predictDir: " + self.predictDir + "\n" + \
+               "dividedImg: " + str(self.dividedImg) + "\n"
+        return text
 
 if __name__ == "__main__":
-    filename = input("파일 입력: ")
-    print(filename)
+    imgProc = ImgProc("../4IRSWContest/assets/image/test/10+12=.png")
 
-    img = cv.imread("./"+ filename +".png")
-    cv.imshow("testing",img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    imgProc.setImg()
+    imgProc.createPredictDir()
 
-    braile_letters = cutting("./"+ filename +".png")
-    for letter in braile_letters:
-        cv.imshow("result",letter)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+    imgProc.cutting()
+
+    imgProc.checkOrigin()
+
+    imgProc.checkPredict(0)
+
+    print(imgProc)
+
+    imgProc.removePredictDir()
+    print(imgProc)
